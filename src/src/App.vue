7ec10474b87851ebf77788c41968afb6901ae49f
Jack Lee <jkprogramador@gmail.com>
2022-04-04 21:43:48 -0300

    Props

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,15 +1,23 @@
 <template>
     <p class="display-4 text-success">Hey!</p>
     <greetingItem></greetingItem>
+    <userItem :age="age"></userItem>
 </template>
 
 <script>
 import GreetingItem from "./components/GreetingItem.vue";
+import UserItem from "./components/UserItem.vue";
 
 export default {
     name: "App",
     components: {
         GreetingItem,
+        UserItem,
+    },
+    data() {
+        return {
+            age: 20,
+        };
     },
 };
 </script>
---
+++ src/src/components/UserItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <p class="display-5">The user is {{ age }} years old</p>
+</template>
+
+<script>
+export default {
+    name: "UserItem",
+    props: ["age"],
+};
+</script>
