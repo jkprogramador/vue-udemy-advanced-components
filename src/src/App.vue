538d4938c53554715d11969994c73bf60e2e770d
Jack Lee <jkprogramador@gmail.com>
2022-04-05 09:59:57 -0300

    Inserting content with slots.

--- src/src/App.vue
+++ src/src/App.vue
@@ -6,17 +6,58 @@
         :ageChangedFn="updateAgeCb"
         @onAgeChanged="updateAge"
     ></userItem>
+    <form-item>
+        <div class="col-12 help">
+            <p>This is some help text</p>
+        </div>
+        <div class="col-md-7 fields">
+            <div class="form-floating mb-3">
+                <input
+                    id="email"
+                    type="text"
+                    class="form-control"
+                    placeholder="Email"
+                />
+                <label for="email">Email</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input
+                    id="username"
+                    class="form-control"
+                    type="text"
+                    placeholder="username"
+                />
+                <label for="username">Username</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input
+                    id="password"
+                    class="form-control"
+                    type="text"
+                    placeholder="password"
+                />
+                <label for="password">Password</label>
+            </div>
+        </div>
+        <div class="col-12 buttons">
+            <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+        </div>
+    </form-item>
+
+    <form-item />
 </template>
 
 <script>
 import GreetingItem from "./components/GreetingItem.vue";
 import UserItem from "./components/UserItem.vue";
+import FormItem from "./components/FormItem.vue";
 
 export default {
     name: "App",
     components: {
         GreetingItem,
         UserItem,
+        FormItem,
     },
     data() {
         return {
---
+++ src/src/components/FormItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <div class="row mt-5">
+            <slot>No form to render</slot>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "FormItem",
+};
+</script>
+
+<style>
+</style>
